3f8996513523682b226e915826d90a7ae579df21
kataya1 <mohamedkataya1@gmail.com>
2020-05-06 04:44:33 +0200

    update and delete

--- app.py
+++ app.py
@@ -1,21 +1,51 @@
-from flask import Flask, render_template, request, json, jsonify, abort
+from flask import Flask, render_template, request, json, jsonify, abort, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 # it create an applictation that gets named after this file which is app in this case, __name__ =  'app'
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:postgres@localhost:5432/todoapp'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+Migrate(app, db)
 
 class Todo(db.Model):
     __tablename__ = 'todo'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr(self):
         return f'Todo {self.id}: {self.description}'
 
-#to insure that the table are created for all the models we made if they haven't yet been created
-db.create_all()
+#to insre that the table are created for all the models we made if they haven't yet been created
+# we don't need it with migrate
+# db.create_all()
+
+@app.route('/todo/<todo_id>/delete_todo', methods=['DELETE'])
+def delete_todo(todo_id):
+    try:
+        db.session.delete(Todo.query.get(todo_id))
+        db.session.commit()
+    except:
+        session_error()
+    finally:
+        db.session.close()
+    return jsonify({'deleted': True})
+
+@app.route('/todo/<todo_id>/set_completed', methods=['POST'])
+def complete(todo_id):
+    try:
+        isCompleted = request.get_json()['completed']
+        todo = Todo.query.get(todo_id)
+        todo.completed = isCompleted
+        db.session.commit()
+    except:
+        session_error()
+    finally:
+        db.session.close()
+    return jsonify({
+        'completed': isCompleted
+    })
 
 def insert_into_db(f1):
     db.session.add(Todo(description=f1))
@@ -25,11 +55,11 @@ def session_error():
     import sys
     db.session.rollback()
     print(sys.exec_info()) 
-
-@app.route('/todos/create', methods=['POST'])
+    
+@app.route('/todo/create', methods=['POST'])
 def create_todo():
     
-    # data_dictionary = request.get_json()['discription] <-- you can use this instead of those three lines
+    # data_dicti = request.get_json()['discription] <-- you can use this instead of those three lines
     data_string = request.data
     data_dictionary = json.loads(data_string)
     x = data_dictionary['description']
@@ -47,14 +77,11 @@ def create_todo():
         return jsonify({'description': x})
     
     
-
-
 @app.route('/')
 def index():
-    d = Todo.query.all()
+    d = Todo.query.order_by('id').all()
     return render_template('index.html', data=d)
-
-
+ 
 
 if __name__ == "__main__":
     app.run()
--- static/js/form.js
+++ static/js/form.js
@@ -2,11 +2,76 @@ let textInput = document.querySelector("#post");
 const inputForm = document.querySelector("form");
 const ul = document.querySelector("ul");
 const errorMessage = document.querySelector("#error")
-inputForm.addEventListener("submit", onSubmit);
+const checkboxes = document.querySelectorAll(".check-completed")
+const deleteButtons = document.querySelectorAll('.delete-button')
 
-function onSubmit(e){
+
+
+
+
+
+
+check_boxes_listen = (checkbox) => {
+    checkbox.addEventListener('change', onCheckboxChange);
+}
+
+delete_buttons_listen = (deleteButton) => {
+    deleteButton.addEventListener('click', onDeleteClick)
+}
+
+
+giveError = (error) => {
+    errorMessage.classList = '';
+    errorMessage.innerHTML = error.message;}
+    
+onDeleteClick = (e) => {
     e.preventDefault()
-    fetch("/todos/create", {
+    console.log(e.target.dataset.id)
+    fetch('/todo/'+ e.target.dataset.id + '/delete_todo', {
+        method: 'DELETE',
+        headers: {
+            "Content-Type": 'application/json'
+        }
+    }).then(res => res.json())
+    .then((data) =>{
+        if(data['deleted']){
+            console.log(e.target.parentElement)
+            e.target.parentElement.remove()
+        }
+    })
+}
+onCheckboxChange = (e) => {
+    e.preventDefault()
+    let checkbox_state = e.target.checked
+    let cbox_id = e.target.dataset['id']
+
+    fetch('/todo/'+ cbox_id + '/set_completed', {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            'completed': checkbox_state
+        })
+    })
+    .then((res) => res.json())
+    .then((data)=> {
+        console.log(data);
+        errorMessage.classList = 'hidden'})
+        .catch(giveError)
+    }
+    
+for(let i=0; i < checkboxes.length; i++)
+{
+    check_boxes_listen(checkboxes[i])
+    delete_buttons_listen(deleteButtons[i])
+}
+
+inputForm.addEventListener("submit", onSubmit);
+function onSubmit(e){
+    console.log(e)
+    e.preventDefault()
+    fetch("/todo/create", {
         method: "POST",
         body: JSON.stringify({
             description: textInput.value
@@ -21,12 +86,13 @@ function onSubmit(e){
     }).then(
         (data) => {
             let l = document.createElement('li');
-            l.innerHTML = textInput.value;
+            // comment tdrsarsrassdtsdtsdtsd
+            
+            l.innerHTML = `${textInput.value}`
+            //function to add onCheckboxChange event litener
             console.log(data);
             ul.appendChild(l);
             errorMessage.classList = 'hidden'
         }
-    ).catch((error) => {
-        errorMessage.classList = '';
-        errorMessage.innerHTML = error.message;})
+    ).catch(giveError)
 }
