c67e6b54c2d1afd3466cb98d74c4c6cbc93a65c0
kataya1 <mohamedkataya1@gmail.com>
2020-05-08 04:55:18 +0200

    complete, weird bugs

--- app.py
+++ app.py
@@ -8,29 +8,47 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 Migrate(app, db)
 
+
 class Todo(db.Model):
     __tablename__ = 'todo'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
-    completed = db.Column(db.Boolean, nullable=False, default=False)
+    completed = db.Column(db.Boolean, default=False)
+    list_id = db.Column(db.Integer, db.ForeignKey('todolist.id'), nullable=False)
 
-    def __repr(self):
-        return f'Todo {self.id}: {self.description}'
+    def __repr__(self):
+        return f'Todo {self.id}: {self.description} (completed: {self.completed})'
+
+
+class TodoList(db.Model):
+    __tablename__ = 'todolist'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    todos = db.relationship('Todo', backref='list', cascade='all, delete-orphan')
 
 #to insre that the table are created for all the models we made if they haven't yet been created
 # we don't need it with migrate
 # db.create_all()
 
-@app.route('/todo/<todo_id>/delete_todo', methods=['DELETE'])
-def delete_todo(todo_id):
+def session_error(e):
+    db.session.rollback()
+    print(e.msg) 
+
+ltdict = {
+    'todo': Todo,
+    'list': TodoList
+}
+@app.route('/delete/<var>/<id>', methods=['DELETE'])
+def delete(var, id):
     try:
-        db.session.delete(Todo.query.get(todo_id))
+        db.session.delete(ltdict[var].query.get(id))
         db.session.commit()
     except:
         session_error()
     finally:
         db.session.close()
     return jsonify({'deleted': True})
+
 
 @app.route('/todo/<todo_id>/set_completed', methods=['POST'])
 def complete(todo_id):
@@ -47,27 +65,22 @@ def complete(todo_id):
         'completed': isCompleted
     })
 
-def insert_into_db(f1):
-    db.session.add(Todo(description=f1))
-    db.session.commit()
-
-def session_error():
-    import sys
-    db.session.rollback()
-    print(sys.exec_info()) 
     
-@app.route('/todo/create', methods=['POST'])
-def create_todo():
-    
+@app.route('/todo/create/<list_id>', methods=['POST'])
+def create_todo(list_id):
     # data_dicti = request.get_json()['discription] <-- you can use this instead of those three lines
     data_string = request.data
     data_dictionary = json.loads(data_string)
     x = data_dictionary['description']
     error = False
     try:
-        insert_into_db(x)
-    except:
-        session_error()
+        t =Todo(description=x)
+        l = TodoList.query.get(list_id)
+        t.list = l
+        db.session.add(t)
+        db.session.commit()
+    except Exception as e:
+        session_error(e)
         error = False
     finally:
         db.session.close()
@@ -75,12 +88,34 @@ def create_todo():
         abort(400)
     else:
         return jsonify({'description': x})
-    
-    
+
+@app.route('/list/create', methods=['POST'])
+def create_list():
+    x = request.get_json()['name'] # <-- you can use this instead of those three lines
+    error = False
+    try:
+        db.session.add(TodoList(name=x))
+        db.session.commit()
+    except Exception as e:
+        session_error(e)
+        error = False
+    finally:
+        db.session.close()
+    if error:
+        abort(400)
+    else:
+        return jsonify({'name': x})
+
+@app.route('/list/<list_id>')   
+def get_todo_list(list_id):
+    d = Todo.query.filter_by(list_id=list_id).order_by('id').all()
+    l = TodoList.query.order_by('id').all()
+    active_list = TodoList.query.get(list_id)
+    return render_template('index.html', todos=d, lists=l, active_list=active_list)
+
 @app.route('/')
 def index():
-    d = Todo.query.order_by('id').all()
-    return render_template('index.html', data=d)
+    return redirect(url_for('get_todo_list', list_id=1))
  
 
 if __name__ == "__main__":
--- static/js/form.js
+++ static/js/form.js
@@ -1,23 +1,19 @@
-let textInput = document.querySelector("#post");
-const inputForm = document.querySelector("form");
-const ul = document.querySelector("ul");
-const errorMessage = document.querySelector("#error")
+const todoTextInput = document.querySelector("#todo_post");
+const todoinputForm = document.querySelector("form.todo");
+const listTextInput = document.querySelector("#list_post")
+const listInputForm = document.querySelector("form.list")
+const ul = document.querySelector("ul.todo");
+const listUl = document.querySelector("ul.list")
+const errorMessage = document.querySelector("#todo_error")
 const checkboxes = document.querySelectorAll(".check-completed")
-const deleteButtons = document.querySelectorAll('.delete-button')
+const deleteButtons = document.querySelectorAll('.delete_button')
+
+const active_list = document.querySelector("#active_list")
 
 
 
 
 
-
-
-check_boxes_listen = (checkbox) => {
-    checkbox.addEventListener('change', onCheckboxChange);
-}
-
-delete_buttons_listen = (deleteButton) => {
-    deleteButton.addEventListener('click', onDeleteClick)
-}
 
 
 giveError = (error) => {
@@ -26,8 +22,9 @@ giveError = (error) => {
     
 onDeleteClick = (e) => {
     e.preventDefault()
-    console.log(e.target.dataset.id)
-    fetch('/todo/'+ e.target.dataset.id + '/delete_todo', {
+    link = `/delete/${e.path[2].classList[0]}/${e.target.dataset.id}`
+    console.log(link)
+    fetch( link, {
         method: 'DELETE',
         headers: {
             "Content-Type": 'application/json'
@@ -42,9 +39,11 @@ onDeleteClick = (e) => {
 }
 onCheckboxChange = (e) => {
     e.preventDefault()
+    console.log(e)
+    
     let checkbox_state = e.target.checked
     let cbox_id = e.target.dataset['id']
-
+    
     fetch('/todo/'+ cbox_id + '/set_completed', {
         method: "POST",
         headers: {
@@ -60,21 +59,36 @@ onCheckboxChange = (e) => {
         errorMessage.classList = 'hidden'})
         .catch(giveError)
     }
-    
-for(let i=0; i < checkboxes.length; i++)
+
+check_boxes_listen = (checkbox) => {
+    checkbox.addEventListener('change', onCheckboxChange);
+}
+
+delete_buttons_listen = (deleteButton) => {
+    deleteButton.addEventListener('click', onDeleteClick)
+}
+// assign event listeners
+for(let i=0; i < deleteButtons.length; i++)
 {
     check_boxes_listen(checkboxes[i])
     delete_buttons_listen(deleteButtons[i])
 }
-
-inputForm.addEventListener("submit", onSubmit);
+    // resolve between todo and list
+    // function resolve(e){
+//     if(e.path[0] === 'input.')
+// }
+todoinputForm.addEventListener("submit", onSubmit);
+listInputForm.addEventListener("submit", onSubmitList);
 function onSubmit(e){
-    console.log(e)
     e.preventDefault()
-    fetch("/todo/create", {
+    console.log(e)
+    active_list_id = active_list.getAttribute('data-id')
+    link = "/todo/create/" + active_list_id
+    console.log("link", link)
+    fetch(link, {
         method: "POST",
         body: JSON.stringify({
-            description: textInput.value
+            description: todoTextInput.value
         }),
         headers: {
             'Content-Type': 'application/json'
@@ -88,10 +102,39 @@ function onSubmit(e){
             let l = document.createElement('li');
             // comment tdrsarsrassdtsdtsdtsd
             
-            l.innerHTML = `${textInput.value}`
+            l.innerHTML = `${todoTextInput.value}`
             //function to add onCheckboxChange event litener
             console.log(data);
             ul.appendChild(l);
+            errorMessage.classList = 'hidden'
+        }
+    ).catch(giveError)
+}
+function onSubmitList(e){
+    e.preventDefault()
+    console.log(e)
+    
+    fetch("/list/create" , {
+        method: "POST",
+        body: JSON.stringify({
+            name: listTextInput.value
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then((res) => {
+        if(res.ok){
+            return res.json()
+        }
+    }).then(
+        (data) => {
+            let l = document.createElement('li');
+            // comment tdrsarsrassdtsdtsdtsd
+            
+            l.innerHTML = `${listTextInput.value}`
+            //function to add onCheckboxChange event litener
+            console.log(data);
+            listUl.appendChild(l);
             errorMessage.classList = 'hidden'
         }
     ).catch(giveError)
